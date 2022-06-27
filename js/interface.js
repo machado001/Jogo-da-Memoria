2ec1b29c1b2deebf949b5e64a736a93571257632
machado001 <machadowg@gmail.com>
2022-06-27 11:42:17 -0300

    changes
    mudei algumas posições no html, pra embaralhar mais.
    refatorei a função embaralhar pois à primeira vista não notei que dava pra colocá-la em um simples loop.

--- js/interface.js
+++ js/interface.js
@@ -14,9 +14,19 @@ const virarCarta = (evento) => {
     }
     console.log(divclick)
 }
+//percorrer cada div e adicionar evento a todas elas
 divs.forEach(div => {
     div.addEventListener('click', virarCarta);
 })
+//embaralhar cards
+(function embaralhar() {
+    for(let i = 0; i<16; i++){
+        const div = document.querySelector(`#div${i}`)
+        let x = div.style.order = Math.floor(Math.random() * 17)
+        console.log(x)
+    }
+}())
+
 
 
 
