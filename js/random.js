7e63afcf0b99fa8232bcc735ace92fd3cc9022a1
machado001 <machadowg@gmail.com>
2022-07-01 04:22:02 -0300

    img bug

--- js/random.js
+++
@@ -1,8 +0,0 @@
-function embaralhar() {
-    for(let i = 0; i<16; i++){
-        const div = document.querySelector(`#div${i}`)
-        let x = div.style.order = Math.floor(Math.random() * 17)
-        console.log(x)
-    }
-}
-embaralhar()
