10fe66036fa5aede6e384f292fc7bedaac9492f2
machado001 <machadowg@gmail.com>
2022-07-01 03:53:46 -0300

    Mais avanços
    Agora. Um cartão vira por vez. E está embaralhado.

--- js/interface.js
+++ js/interface.js
@@ -11,9 +11,9 @@ const personagens = [
     'momonosuke',
     'law',
     'sabo',
-    'ace'
+    'ace',
 ]
-const duplicatePersonagens = [...personagens, ...personagens]
+const duplicatedPersonagens = [...personagens, ...personagens]
 const virarCarta = ({ target }) => {
     const box = target;
     const boxClass = box.classList;
@@ -21,58 +21,47 @@ const virarCarta = ({ target }) => {
     if (!divclick) {
         setTimeout(() => {
             let img = box.children[0]
-            img.style.display ='inline-block'
-            img.src= `/images/${personagens[Math.floor(Math.random() * 8)]}.png`
-            console.log(img)
+            img.style.display = 'inline-block'
+            
         }, 295);
     }
 }
 const criarHTML = () => {
     const main = document.createElement('main')
     const body = document.querySelector('body')
+    const elementsDuplicated = duplicatedPersonagens.length
     main.id = 'table'
     body.insertAdjacentElement('afterbegin', main)
-    //criardivs
-    for (let i = 0; i < 1; i++) {
-        for(let j = 0; j <duplicatePersonagens.length; j++){
+    //criar
+    for (let i = 0; i < 2; i++) {
+        for (let j = 0; j < personagens.length; j++) {
             const div = document.createElement('div')
             const img = document.createElement('img')
             div.classList.add(`a${j}`, 'box')
             img.classList.add(`a${j}`)
+            div.style.order = Math.floor(Math.random() * elementsDuplicated) //random imgs
+            img.src = `/images/${personagens[j]}.png`
             main.appendChild(div)
             div.appendChild(img)
-
         }
     }
-    // for (let i = 0; i < 8; i++) {
-    //     const div = document.createElement('div')
-    //     const img = document.createElement('img')
-    //     div.classList.add(`a${i}`, 'box')
-    //     img.classList.add(`a${i}`)
-    //     main.appendChild(div)
-    //     div.appendChild(img)
-    // }
-    // for (let i = 0; i < 8; i++) {
-    //     const div = document.createElement('div')
-    //     const img = document.createElement('img')
-    //     div.classList.add(`a${i}`, 'box')
-    //     img.classList.add(`a${i}`)
-    //     main.appendChild(div)
-    //     div.appendChild(img)
-    // }
+
     const divs = document.querySelectorAll('.box')
+
     divs.forEach(div => div.addEventListener('click', virarCarta))
 }
 
 document.addEventListener('DOMContentLoaded', criarHTML)
-    //percorrer cada div e adicionar evento a todas elas
-    //embaralhar cards
-    // (function embaralhar() {
-    //     for (let i = 0; i < 25; i++) {
-    //         const div = document.querySelector(`#div${i}`)
-    //         div.style.order = Math.floor(Math.random() * 17)
-    //     }
-    // }())
+//percorrer cada div e adicionar evento a todas elas
+//embaralhar cards
+function embaralhar() {
+    let elementsDuplicated = duplicatedPersonagens.length
+    for (let i = 0; i < elementsDuplicated; i++) {
+        const div = document.querySelector(`.a${i}`)
+        console.log(div)
+        div.style.order = Math.floor(Math.random() * elementsDuplicated)
+    }
+}
 
 
 
